2626415dfc1afc4db5722117195fd3d68ebf2a38
Mike <miketorreno7@gmail.com>
2025-07-21 21:19:50 +0300

    feat(theme): add theme provider and toggle functionality

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { AppSidebar } from "@/components/app-sidebar";
 import {
   Breadcrumb,
@@ -7,14 +9,25 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { Separator } from "@/components/ui/separator";
 import {
   SidebarInset,
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
 
 const AppLayout = ({ children }: { children: React.ReactNode }) => {
+  const { setTheme } = useTheme();
+
   return (
     <SidebarProvider>
       <AppSidebar />
@@ -38,8 +51,29 @@ const AppLayout = ({ children }: { children: React.ReactNode }) => {
               </BreadcrumbList>
             </Breadcrumb>
           </div>
+
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" size="icon">
+                <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+                <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+                <span className="sr-only">Toggle theme</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem onClick={() => setTheme("light")}>
+                Light
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setTheme("dark")}>
+                Dark
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setTheme("system")}>
+                System
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </header>
-        <div className="p-6 bg-gray-100 min-h-10/12">{children}</div>
+        <div className="p-6 min-h-10/12">{children}</div>
       </SidebarInset>
     </SidebarProvider>
   );
